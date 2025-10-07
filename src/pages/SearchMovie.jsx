ab2d2bda33badcf879274fae024909b35385a97f
OlexandrBilyk <0500423510s@gmail.com>
2025-10-07 23:36:04 +0300

    fix

--- src/pages/SearchMovie.jsx
+++ src/pages/SearchMovie.jsx
@@ -12,9 +12,7 @@ export default function SearchMovie() {
 
     try {
       const response = await axios.get(
-        `https://api.themoviedb.org/3/search/multi?api_key=5bc2d8b1a6e814e03b1c29b7fa3a3756&query=${encodeURIComponent(
-          query
-        )}`
+        `https://api.themoviedb.org/3/search/multi?api_key=5bc2d8b1a6e814e03b1c29b7fa3a3756&query=${query}`
       );
 
       setSearched(response.data.results);
